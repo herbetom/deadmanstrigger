8a8cb14f4f7c3f3916d97006ccf4c83dbc68471c
Tom Herbers <github@herbetom.de>
2018-09-14 01:39:00 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Dead Man's Trigger</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+        <link rel="icon" type="image/png" href="/skull_icon_32px.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/skull_icon_96px.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="/skull_icon_512px.png" sizes="512x512">
+        <link rel="stylesheet" href="/style.css?v=3">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="Dead Man's Trigger">
+        <meta name="application-name" content="Dead Man's Trigger">
+        <meta name="msapplication-TileColor" content="#000000">
+        <meta name="theme-color" content="#000">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color: #000000;">
+<!--        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">-->
+            <a class="navbar-brand" href="/">
+                <img src="/skull_icon_white.svg?v2" width="30px" height="30px" class="d-inline-block align-top" alt="">
+                Dead Man's Trigger
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://tomhe.de/impressum.html">Impressum</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <main role="main" class="container">
+            <h1>Dead Man's Trigger</h1>
+            <p class="lead">Content follows soon!</p>
+            <p class="">This Service will allow you to configure it that way, that if after a given timespane you don't respond a mail will be send to someone you trust.</p>
+            <p class="">Contact me under: <a href="mailto:info@deadmanstrigger.de">info@deadmanstrigger.de</a></p>
+<!--                <img src="/skull_icon_512px.png">-->
+            <img src="/skull_icon.svg?2" width="512px" style="max-width: 100%;" class="d-inline-block align-top" alt="">
+        </main>
+        <footer class="footer">
+            <div class="left">
+                <a href="/">
+                    <img src="/skull_icon.svg">
+                    Dead Man's Trigger
+                </a>
+            </div>
+            <div class="right">
+                <a href="https://github.com/herbetom/deadmanstrigger" target="_blank">This Software is Open Source <i class="fa fa-github fa-lg"></i></a>
+            </div>
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
